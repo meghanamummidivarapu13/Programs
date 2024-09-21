3d390ea850f838d3bf8994b97f77f4527e04fc76
Meghana <meghanamummidivarapu@gmail.com>
2024-09-21 13:53:47 +0530

    Update program1.java
    Basic

--- program1.java
+++ program1.java
@@ -1,5 +1,10 @@
 class A 
 {
-  int a =10;
-  System.out.println("a");
+ public static int a =10;
+  public int j=20;
+  public static viod main(String[]args)
+  {
+        System.out.println(a);
+        A a1=new A();
+        System.out.println(a1.j);
 }
